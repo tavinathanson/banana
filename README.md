082da68c857ae0c58e1e89f62a90b65584b2c4aa
Tavi Nathanson <tavi.nathanson@gmail.com>
2016-07-12 15:42:27 -0400

    First version of banana

---
+++ README.md
@@ -0,0 +1,4 @@
+Banana
+======
+
+Banana is a Chrome Extension for more accurately tracking code snippets in GitHub PR diffs.
---
+++ banana.js
@@ -0,0 +1,25 @@
+if ("https://github.com" === window.location.origin) {
+    var minus_lines = [];
+    var plus_lines = [];
+    skip_lines = ["", "(", ")", "{", "}"];
+    line_text_to_element_plus = {};
+    line_text_to_element_minus = {};
+    $('.diff-table .blob-code-deletion').each(function(index, line) {
+        var line_text = $.trim($.trim($(line).text()).substring(1));
+        if ($.inArray(line_text, skip_lines) == -1) { minus_lines.push(line_text); }
+        line_text_to_element_minus[line_text] = line;
+    });
+    $('.diff-table .blob-code-addition').each(function(index, line) {
+        var line_text = $.trim($.trim($(line).text()).substring(1));
+        if ($.inArray(line_text, skip_lines) == -1) { plus_lines.push(line_text); }
+        line_text_to_element_plus[line_text] = line;
+    });
+    $(minus_lines).each(function(index, line) {
+        if ($.inArray(line, plus_lines) !== -1) {
+            elem = line_text_to_element_plus[line]
+            if (elem.className.indexOf("code-yellow") === -1) {
+                line_text_to_element_plus[line].className = "blob-code blob-code-banana";
+            }
+        }
+    });
+}
